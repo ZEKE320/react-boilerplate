5e2a2374ef20afa1d5a46404674f2471d778eba0
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-03-16 20:55:45 +0900

    ✨ TypeScript(React)開発全般の環境設定ファイルを整備

---
+++ ts/util/.eslintrc.js
@@ -0,0 +1,37 @@
+import { stylistic } from "@stylistic/eslint-plugin";
+const stylisticConfigs = stylistic.configs["recommended-extends"];
+
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react/jsx-a11y-recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "@stylistic", "react", "prettier"],
+  rules: {
+    ...stylisticConfigs.rules,
+    "@stylistic/indent": ["error", 4],
+    "@stylistic/linebreak-style": ["error", "unix"],
+    "@stylistic/quotes": ["error", "double"],
+    "@stylistic/semi": ["error"],
+    "@stylistic/strict-boolean-expressions": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["dist", "node_modules"],
+};
